ca72c1a8e43509546fe14d89d9ae4187d9875db6
unknown <Nightmachine@gmail.com>
2018-08-27 23:48:49 -0600

    added a motherboard to products

--- db/seed.sql
+++ db/seed.sql
@@ -31,6 +31,13 @@ add products
 -- (product, price, description, image_url)
 -- values
 -- ('Monitor', 200.00, '32 inch ultrawide monitor from Samsung', 'https://static1.squarespace.com/static/576bf6f9197aeaf55ed0447d/5998cdd3be42d6f75f9a99ed/5b817fd44fa51af3faf2ea8c/1535406751162/24-009-847-16.jpg?format=750w')
+-------------
+-- insert into products
+-- (product, price, description, image_url)
+-- values
+-- ('Motherboard', 130.00 , 'ASRock Z370 Extreme4 LGA 1151 (300 Series) Intel Z370 HDMI SATA 6Gb/s USB 3.1 ATX Intel Motherboard', 'https://c1.neweggimages.com/NeweggImage/ProductImageCompressAll1280/13-157-789-V01.jpg')
+
+
 
 
 -----------------------------------
--- src/Components/Home/Home.js
+++ src/Components/Home/Home.js
@@ -22,42 +22,29 @@ export default class Home extends Component {
             console.log(res.data)
         })
     }
-
-
-
     render() {
 
         let productsToDisplay = this.state.products.map((product, i) => {
             return (
                 <Tile_builder
-                product={product}
-                
-                
+                product={product}               
                  />
             )
         })
         return (
             <div>
                 <h1>Home</h1>
-
-                {productsToDisplay}
-                
-                
-             
+                {productsToDisplay}           
                 <nav>
                     <ul>
-
                         <Link to='/audio'><button>Audio</button></Link>
                         <Link to='/displays'><button>Displays</button></Link>
                         <Link to='/games'><button>Games</button></Link>
                         <Link to='/misc'><button>Misc</button></Link>
                         <Link to='/parts'><button>PC Parts</button></Link>
                         <Link to='/peripherals'><button>Peripherals</button></Link>
-
                     </ul>
                 </nav>
-
-
             </div>
         )
     }
