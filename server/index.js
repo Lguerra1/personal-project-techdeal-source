55830c1d98d663e401847f8501568b90e0e10b75
Larry <Nightmachine@gmail.com>
2018-09-13 16:10:48 -0600

    changed the autho env variable

--- server/index.js
+++ server/index.js
@@ -36,7 +36,8 @@ app.get('/auth/callback', async (req, res) => {
         client_secret: CLIENT_SECRET,
         code: req.query.code,
         grant_type: 'authorization_code',
-        redirect_uri: `http://${req.headers.host}/auth/callback`
+        redirect_uri: process.env.AUTH_URI
+        
     }
 
     //trading code for token
